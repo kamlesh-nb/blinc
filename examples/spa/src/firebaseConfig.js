6ab799dca3e0fc5839e45642874c7a285e561016
kamlesh <kamlesh_nb@yahoo.com>
2020-06-09 12:21:37 +0530

    ...

--- examples/spa/src/firebaseConfig.js
+++ examples/spa/src/firebaseConfig.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdv4zaLrUcAIvjV39OI4vU9qpTI14CUQE",
-  authDomain: "blinc-demo.firebaseapp.com",
-  databaseURL: "https://blinc-demo.firebaseio.com",
-  projectId: "blinc-demo",
-  storageBucket: "blinc-demo.appspot.com",
-  messagingSenderId: "917564745815",
-  appId: "1:917564745815:web:6e084b7d56b585ceba9b12",
-  measurementId: "G-KBS7XZX95Z",
+ 
 };
 
 const app = firebase.initializeApp(firebaseConfig)
